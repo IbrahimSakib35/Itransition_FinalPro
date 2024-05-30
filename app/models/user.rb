e760226cf72f471b99e513c50faa108849aad78d
IbrahimSakib35 <128493670+IbrahimSakib35@users.noreply.github.com>
2024-05-30 14:14:49 +0600

    css

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
   has_many :collections
+  has_many :collections, dependent: :destroy
+  has_many :items, through: :collections, dependent: :destroy
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
